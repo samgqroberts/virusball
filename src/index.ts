ca7e6427e5bf9f9378efd70e1c66b5c8c1e68395
Sam Roberts <samgqroberts@gmail.com>
2020-01-15 22:57:43 -0500

    add canvas, initialize webgl context

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 
-alert('hello world!');
+const canvasId = 'canvas';
+
+const canvas = document.getElementById(canvasId) as HTMLCanvasElement | null;
+if (!canvas) {
+  throw new Error(`Could not find canvas, using id: '${canvasId}'`);
+}
+
+const gl = canvas.getContext('webgl');
+if (!gl) {
+  throw new Error(`Could not get webgl context from canvas`);
+}
+
+// Set clear color to black, fully opaque
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+// Clear the color buffer with specified clear color
+gl.clear(gl.COLOR_BUFFER_BIT);
+
