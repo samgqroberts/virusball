082999be7880a1917784446a2ef562642c1b4c6e
Sam Roberts <samgqroberts@gmail.com>
2020-02-02 00:34:01 -0500

    show a single circle with proper circular proportions
    - remove tutorial square considerations, clean up code around that

--- src/index.ts
+++ src/index.ts
@@ -19,28 +19,14 @@ globalGL.clearColor(0.0, 0.0, 0.0, 1.0);
 // Clear the color buffer with specified clear color
 globalGL.clear(globalGL.COLOR_BUFFER_BIT);
 
-const tutorialSquareVsSource = `
-  attribute vec4 aVertexPosition;
-
+const circleVsSource = `
+  attribute vec4 aPosition;
+  
   uniform mat4 uModelViewMatrix;
   uniform mat4 uProjectionMatrix;
 
   void main() {
-    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-  }
-`;
-
-const tutorialSquareFsSource = `
-  void main() {
-    gl_FragColor = vec4(.5, 0.5, 1.0, 1.0);
-  }
-`;
-
-const circleVsSource = `
-  attribute vec4 aPosition;
-
-  void main() {
-    gl_Position = aPosition;
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aPosition;
   }
 `;
 
@@ -105,20 +91,6 @@ function initShaderProgramOrFail(gl: WebGLRenderingContext, vsSource: string, fs
     || (() => { throw new Error('could not initShaderProgram'); })();
 }
 
-const tutorialSquareProgram =
-  initShaderProgramOrFail(globalGL, tutorialSquareVsSource, tutorialSquareFsSource);
-
-const tutorialSquareProgramInfo = {
-  program: tutorialSquareProgram,
-  attribLocations: {
-    vertexPosition: globalGL.getAttribLocation(tutorialSquareProgram, 'aVertexPosition'),
-  },
-  uniformLocations: {
-    projectionMatrix: globalGL.getUniformLocation(tutorialSquareProgram, 'uProjectionMatrix'),
-    modelViewMatrix: globalGL.getUniformLocation(tutorialSquareProgram, 'uModelViewMatrix'),
-  },
-};
-
 const circleProgram = initShaderProgramOrFail(globalGL, circleVsSource, circleFsSource);
 
 const circleProgramInfo = {
@@ -126,188 +98,56 @@ const circleProgramInfo = {
   attribLocations: {
     vertexPosition: globalGL.getAttribLocation(circleProgram, 'aPosition'),
   },
+  uniformLocations: {
+    projectionMatrix: globalGL.getUniformLocation(circleProgram, 'uProjectionMatrix'),
+    modelViewMatrix: globalGL.getUniformLocation(circleProgram, 'uModelViewMatrix'),
+  },
 };
 
-interface TutorialSquareBuffers {
-  position: WebGLBuffer
-}
-function initTutorialSquareBuffers(gl: WebGLRenderingContext): TutorialSquareBuffers {
-
-  // Create a buffer for the square's positions.
-
-  const positionBuffer = gl.createBuffer()
-    || (() => { throw new Error('could not create buffer'); })();
-
-  // Select the positionBuffer as the one to apply buffer
-  // operations to from here out.
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  // Now create an array of positions for the square.
-
-  const positions = [
-    -1.0,  1.0,
-     1.0,  1.0,
-    -1.0, -1.0,
-     1.0, -1.0,
-  ];
-
-  // Now pass the list of positions into WebGL to build the
-  // shape. We do this by creating a Float32Array from the
-  // JavaScript array, then use it to fill the current buffer.
-
-  gl.bufferData(gl.ARRAY_BUFFER,
-                new Float32Array(positions),
-                gl.STATIC_DRAW);
-
-  return {
-    position: positionBuffer,
-  };
-}
-function initCircleBuffers(
+function initCircleBuffer(
   gl: WebGLRenderingContext,
   pInfo: typeof circleProgramInfo,
 ) {
   const program = pInfo.program;
+  gl.useProgram(program);
   // Create a buffer object
   const vertexBuffer = gl.createBuffer();
   let vertices: number[] = [];
   const vertCount = 2;
-  for (var i=0.0; i<=330; i+=1) {
+  for (let i = 0.0; i <= 360; i+=1) {
     // degrees to radians
-    var j = i * Math.PI / 180;
+    let j = i * Math.PI / 180;
     // X Y Z
-    var vert1 = [
+    let vert1 = [
       Math.sin(j),
       Math.cos(j),
       // 0,
     ];
-    var vert2 = [
+    let vert2 = [
       0,
       0,
-      // 0,
     ];
-    // DONUT:
-    // var vert2 = [
-    //   Math.sin(j)*0.5,
-    //   Math.cos(j)*0.5,
-    // ];
     vertices = vertices.concat(vert1);
     vertices = vertices.concat(vert2);
   }
-  var n = vertices.length / vertCount;
+
+  // TODO what exactly "n" is should be propagated up to rendering
+  let n = vertices.length / vertCount;
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-  var aPosition = pInfo.attribLocations.vertexPosition;
-  gl.enableVertexAttribArray(aPosition);
-  gl.vertexAttribPointer(aPosition, vertCount, gl.FLOAT, false, 0, 0);
-
-  return n;
-}
-function initBuffers(
-  gl: WebGLRenderingContext
-) {
-  return initTutorialSquareBuffers(gl);
-  // TODO currently circle buffers are inited on each draw
-  //      we should have them only initialize once for perf
-  // initCircleBuffers(gl, circleProgram);
-}
-
-let squareRotation = 0.0;
-
-function drawTutorialSquare(
-  gl: WebGLRenderingContext,
-  pInfo: typeof tutorialSquareProgramInfo,
-  deltaTime: number,
-  positionCorrectX: number
-) {
-
-  // Set the drawing position to the "identity" point, which is
-  // the center of the scene.
-  const modelViewMatrix = mat4.create();
-
-  // Now move the drawing position a bit to where we want to
-  // start drawing the square.
-
-  mat4.translate(modelViewMatrix,     // destination matrix
-    modelViewMatrix,     // matrix to translate
-    [-0.0 + positionCorrectX, 0.0, -6.0]);  // amount to translate
-  squareRotation += deltaTime;
-  mat4.rotate(modelViewMatrix,  // destination matrix
-    modelViewMatrix,  // matrix to rotate
-    squareRotation,   // amount to rotate in radians
-    [0, 0, 1]);       // axis to rotate around
-
-  // Tell WebGL how to pull out the positions from the position
-  // buffer into the vertexPosition attribute.
-  {
-    const numComponents = 2;  // pull out 2 values per iteration
-    const type = gl.FLOAT;    // the data in the buffer is 32bit floats
-    const normalize = false;  // don't normalize
-    const stride = 0;         // how many bytes to get from one set of values to the next
-                              // 0 = use type and numComponents above
-    const offset = 0;         // how many bytes inside the buffer to start from
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-    gl.vertexAttribPointer(
-      pInfo.attribLocations.vertexPosition,
-      numComponents,
-      type,
-      normalize,
-      stride,
-      offset);
-    gl.enableVertexAttribArray(
-      pInfo.attribLocations.vertexPosition);
-  }
-
-  gl.uniformMatrix4fv(
-    pInfo.uniformLocations.modelViewMatrix,
-    false,
-    modelViewMatrix);
-
-  {
-    const offset = 0;
-    const vertexCount = 4;
-    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
-  }
+  return vertexBuffer;
 }
 
 function drawCircle(
   gl: WebGLRenderingContext,
-  pInfo: typeof circleProgramInfo
+  program: WebGLProgram,
+  buffer: WebGLBuffer,
+  vertexPositionLocation: number,
+  projectionMatrixLocation: WebGLUniformLocation,
+  modelViewMatrixLocation: WebGLUniformLocation,
 ) {
-  gl.useProgram(pInfo.program);
-  // Write the positions of vertices to a vertex shader
-  var n = initCircleBuffers(gl, pInfo);
-  if (n < 0) {
-    console.log('Failed to set the positions of the vertices');
-    return;
-  }
-
-  // Specify the color for clearing <canvas>
-  // gl.clearColor(0, 0, 0, 1);
-  // gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // Draw a line
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
-}
-
-function drawScene(
-  gl: WebGLRenderingContext,
-  tutorialSquarePInfo: typeof tutorialSquareProgramInfo,
-  circlePInfo: typeof circleProgramInfo,
-  buffers: TutorialSquareBuffers,
-  deltaTime: number,
-  squarePosCorrectX: number
-) {
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-  gl.clearDepth(1.0);                 // Clear everything
-  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
-
-  // Clear the canvas before we start drawing on it.
-
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.useProgram(program);
 
   // Create a perspective matrix, a special matrix that is
   // used to simulate the distortion of perspective in a camera.
@@ -323,12 +163,6 @@ function drawScene(
   const zFar = 100.0;
   const projectionMatrix = mat4.create();
 
-  /*
-   * set the projection
-   * this uses the same program as the tutorial square
-   * when removing the tutorial square we will need to preserve this element, i think
-   */
-
   // note: gl-matrix.js always has the first argument
   // as the destination to receive the result.
   mat4.perspective(projectionMatrix,
@@ -337,23 +171,67 @@ function drawScene(
     zNear,
     zFar);
 
-  // Tell WebGL to use our program when drawing
-
-  gl.useProgram(tutorialSquarePInfo.program);
-
-  // Set the shader uniforms
-
+  // set this matrix to the value in the program uniform
   gl.uniformMatrix4fv(
-    tutorialSquarePInfo.uniformLocations.projectionMatrix,
+    projectionMatrixLocation,
     false,
     projectionMatrix);
 
-  drawTutorialSquare(gl, tutorialSquarePInfo, deltaTime, squarePosCorrectX);
+  // Set the drawing position to the "identity" point, which is
+  // the center of the scene.
+  const modelViewMatrix = mat4.create();
 
-  drawCircle(gl, circlePInfo);
+  // Now move the drawing position a bit to where we want to
+  // start drawing the square.
+
+  mat4.translate(modelViewMatrix,     // destination matrix
+    modelViewMatrix,     // matrix to translate
+    [-0.0, 0.0, -6.0]);  // amount to translate
+  // note: translating "back" 6 units (putting the circle 6 units deep) makes it appear smaller
+
+  // set matrix to program uniform
+  gl.uniformMatrix4fv(
+    modelViewMatrixLocation,
+    false,
+    modelViewMatrix);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  let aPosition = vertexPositionLocation;
+  gl.enableVertexAttribArray(aPosition);
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+
+  // Draw the polygons in the circle buffer
+  // TODO 722 here is a magic number - it should be retrieved from initCircleBuffer
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 722);
 }
 
-const buffers = initBuffers(globalGL);
+function drawScene(
+  gl: WebGLRenderingContext,
+  circlePInfo: typeof circleProgramInfo,
+  circleBuffers: WebGLBuffer,
+) {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+  gl.clearDepth(1.0);                 // Clear everything
+  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+
+  // Clear the canvas before we start drawing on it.
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  if (!circlePInfo.uniformLocations.projectionMatrix) {
+    throw new Error('projectionMatrix location not set');
+  }
+  if (!circlePInfo.uniformLocations.modelViewMatrix) {
+    throw new Error('modelViewMatrix location not set');
+  }
+
+  drawCircle(gl, circlePInfo.program, circleBuffers, circlePInfo.attribLocations.vertexPosition,
+    circlePInfo.uniformLocations.projectionMatrix,
+    circlePInfo.uniformLocations.modelViewMatrix);
+}
+
+const globalCircleBuffers = initCircleBuffer(globalGL, circleProgramInfo)
+  || (() => { throw new Error('Could not init circle buffers'); })();
 
 let then = 0;
 
@@ -384,7 +262,7 @@ function tick(gl: WebGLRenderingContext, now: number) {
     tutorialSquarePositionCorrectionX += 1;
   }
 
-  drawScene(gl, tutorialSquareProgramInfo, circleProgramInfo, buffers, deltaTime, tutorialSquarePositionCorrectionX);
+  drawScene(gl, circleProgramInfo, globalCircleBuffers);
 
   requestAnimationFrame((now: number) => tick(gl, now));
 }
